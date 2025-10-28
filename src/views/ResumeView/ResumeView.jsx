ebf947259085ba8869d0b7137937b40973b9b8f7
Kyle Thurin <KyleThurin01@gmail.com>
2025-10-28 17:52:57 -0500

    Updated ResumeView

--- src/views/ResumeView/ResumeView.jsx
+++ src/views/ResumeView/ResumeView.jsx
@@ -6,11 +6,11 @@ export default function ResumeView() {
     return (
         <>
             <h3>Kyle Thurin's Resume:</h3>
-            
+
             <iframe src={resumePdf} className={styles.iframe} />
 
             <p>
-                <a href={resumePdf} target="_blank" rel="noopener noreferrer" download>
+                <a href={resumePdf} target="_blank" rel="noopener noreferrer" download className={styles.downloadButton}>
                     Download Resume (PDF)
                 </a>
             </p>
